7d630a772a4fca59495896657c66853cf832ee9f
MatPreuter <MatPreuter@users.noreply.github.com>
2024-02-14 15:27:40 +0100

    Update RemoveCipheredLSPlugin.js

--- www/RemoveCipheredLSPlugin.js
+++ www/RemoveCipheredLSPlugin.js
@@ -1,33 +1,33 @@
 // Override existing openDatabase to automaticly cdelete and recreate a sqllte database when it is corrupt
 var originalOpenDatabase = window.sqlitePlugin.openDatabase;
 window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
-
+ 
     return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, function() {
-
+ 
         var now = new Date();
         now.setTime(now.getTime() + 1 * 3600 * 1000);
         var dbObj = { name: ApplicationInfo.getDatabaseName(), location: "default" };
-
+ 
         sqlitePlugin.openDatabase(dbObj, function(db) {
-       
-        // Tests the connection to SQL Lite
-        db.transaction(function(tx) {
-            tx.addStatement("PRAGMA journal_mode;", [], function() { 
-                // Success - connection is working, no need to upgrade
-
-            }, function() {
-                // Error - connection is not working, likely the database file is corrupt
-                // Delete the database, and reload the screen so a new empty database will be created
-
-                sqlitePlugin.deleteDatabase(dbObj, function() {
-                    var now = new Date();
-                    now.setTime(now.getTime() + 1 * 3600 * 1000);
-
-                    document.cookie = "NewDatabaseIsCreated=1; expires=" + now.toUTCString() + "; path=/";
-
-                    sqlitePlugin.openDatabase(options, successCallback, errorCallback);
-                });
-            });
-        })
-    });
+			// Tests the connection to SQL Lite
+			db.transaction(function(tx) {
+				tx.addStatement("PRAGMA journal_mode;", [], function() { 
+					// Success - connection is working, no need to upgrade
+ 
+				}, function() {
+					// Error - connection is not working, likely the database file is corrupt
+					// Delete the database, and reload the screen so a new empty database will be created
+ 
+					sqlitePlugin.deleteDatabase(dbObj, function() {
+						var now = new Date();
+						now.setTime(now.getTime() + 1 * 3600 * 1000);
+ 
+						document.cookie = "NewDatabaseIsCreated=1; expires=" + now.toUTCString() + "; path=/";
+ 
+						sqlitePlugin.openDatabase(options, successCallback, errorCallback);
+					});
+				});
+			});
+		});
+	});
 };
