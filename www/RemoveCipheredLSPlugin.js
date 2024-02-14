0b75a7ba80e6751b1f530520531c100c0ef6503c
MatPreuter <matthias@preuter.eu>
2024-02-14 17:24:32 +0100

    Update RemoveCipheredLSPlugin.js

--- www/RemoveCipheredLSPlugin.js
+++ www/RemoveCipheredLSPlugin.js
@@ -1,14 +1,26 @@
-// Override existing openDatabase to automaticly cdelete and recreate a sqllte database when it is corrupt
-
+// Override existing openDatabase to automatically provide the `key` option
 var originalOpenDatabase = window.sqlitePlugin.openDatabase;
 window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
+    return acquireLsk(
+        function (key) {
+            // Clone the options
+            var newOptions = {};
+            for (var prop in options) {
+                if (options.hasOwnProperty(prop)) {
+                    newOptions[prop] = options[prop];
+                }
+            }
+            
+            // Ensure `location` is set (it is mandatory now)
+            if (newOptions.location === undefined) {
+                newOptions.location = "default";
+            }
+            
+            // Set the `key` to the one provided
+            newOptions.key = key;
 
-    return originalOpenDatabase.call(window.sqlitePlugin, options, successCallback, function() {
-
-        var now = new Date();
-        now.setTime(now.getTime() + 1 * 3600 * 1000);
-        var dbObj = { name: ApplicationInfo.getDatabaseName(), location: "default" };
-
+            // Validate the options and call the original openDatabase
+            validateDbOptions(newOptions);
             return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, function() {
 		    sqlitePlugin.deleteDatabase(options, function() {
 			    window.sqlitePlugin.openDatabase(options, successCallback, errorCallback);
