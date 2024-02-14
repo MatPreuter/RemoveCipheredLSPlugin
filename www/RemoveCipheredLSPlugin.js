63a9706fc6c486ef1259a55dc9c0952d552a5033
MatPreuter <MatPreuter@users.noreply.github.com>
2024-02-14 15:46:17 +0100

    Update RemoveCipheredLSPlugin.js

--- www/RemoveCipheredLSPlugin.js
+++ www/RemoveCipheredLSPlugin.js
@@ -7,27 +7,20 @@ window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallb
         var now = new Date();
         now.setTime(now.getTime() + 1 * 3600 * 1000);
         var dbObj = { name: ApplicationInfo.getDatabaseName(), location: "default" };
- 
-        sqlitePlugin.openDatabase(dbObj, function(db) {
-			// Tests the connection to SQL Lite
-			db.transaction(function(tx) {
-				tx.addStatement("PRAGMA journal_mode;", [], function() { 
-					// Success - connection is working, no need to upgrade
- 
-				}, function() {
-					// Error - connection is not working, likely the database file is corrupt
-					// Delete the database, and reload the screen so a new empty database will be created
- 
-					sqlitePlugin.deleteDatabase(dbObj, function() {
-						var now = new Date();
-						now.setTime(now.getTime() + 1 * 3600 * 1000);
- 
-						document.cookie = "NewDatabaseIsCreated=1; expires=" + now.toUTCString() + "; path=/";
- 
-						sqlitePlugin.openDatabase(options, successCallback, errorCallback);
-					});
-				});
-			});
-		});
-	});
+
+
+            return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, function() {
+		    sqlitePlugin.deleteDatabase(options, function() {
+				var now = new Date();
+				now.setTime(now.getTime() + 1 * 3600 * 1000);
+
+				document.cookie = "NewDatabaseIsCreated=1; expires=" + now.toUTCString() + "; path=/";
+				
+			    window.sqlitePlugin.openDatabase(options, successCallback, errorCallback);
+		    }, function() {
+			    errorCallback();
+		    });
+	    });
+        },
+        errorCallback);
 };
