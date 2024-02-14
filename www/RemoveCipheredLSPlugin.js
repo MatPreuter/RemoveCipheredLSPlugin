fb3f7a9eb17141d69e956b57d8a53a85146f584f
MatPreuter <MatPreuter@users.noreply.github.com>
2024-02-14 15:57:04 +0100

    Update RemoveCipheredLSPlugin.js

--- www/RemoveCipheredLSPlugin.js
+++ www/RemoveCipheredLSPlugin.js
@@ -3,17 +3,17 @@ var originalOpenDatabase = window.sqlitePlugin.openDatabase;
 window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
  
     return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, function() {
- 
+
         var now = new Date();
         now.setTime(now.getTime() + 1 * 3600 * 1000);
         var dbObj = { name: ApplicationInfo.getDatabaseName(), location: "default" };
 
 
-            return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, function() {
+	    return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, function() {
 		    sqlitePlugin.deleteDatabase(options, function() {
 				var now = new Date();
 				now.setTime(now.getTime() + 1 * 3600 * 1000);
-
+	
 				document.cookie = "NewDatabaseIsCreated=1; expires=" + now.toUTCString() + "; path=/";
 				
 			    window.sqlitePlugin.openDatabase(options, successCallback, errorCallback);
