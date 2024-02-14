ee73f04c50e5d83f09c6bf85159c3b8c0fbd9761
MatPreuter <MatPreuter@users.noreply.github.com>
2024-02-14 16:07:50 +0100

    Update RemoveCipheredLSPlugin.js

--- www/RemoveCipheredLSPlugin.js
+++ www/RemoveCipheredLSPlugin.js
@@ -1,16 +1,17 @@
 // Override existing openDatabase to automaticly cdelete and recreate a sqllte database when it is corrupt
+
+var ApplicationInfo = require("OutSystems/ClientRuntime/ApplicationInfo")
 var originalOpenDatabase = window.sqlitePlugin.openDatabase;
 window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
  
-    return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, function() {
+    return originalOpenDatabase.call(window.sqlitePlugin, options, successCallback, function() {
 
         var now = new Date();
         now.setTime(now.getTime() + 1 * 3600 * 1000);
         var dbObj = { name: ApplicationInfo.getDatabaseName(), location: "default" };
 
-
-	    return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, function() {
-		    sqlitePlugin.deleteDatabase(options, function() {
+	    return originalOpenDatabase.call(window.sqlitePlugin, options, successCallback, function() {
+		    window.sqlitePlugin.deleteDatabase(options, function() {
 				var now = new Date();
 				now.setTime(now.getTime() + 1 * 3600 * 1000);
 	
