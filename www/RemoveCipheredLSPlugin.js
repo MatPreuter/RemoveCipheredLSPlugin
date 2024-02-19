0a88b9a641e8860951aecacfd8ce5e195ef91b8e
MatPreuter <MatPreuter@users.noreply.github.com>
2024-02-19 08:49:50 +0100

    Update RemoveCipheredLSPlugin.js

--- www/RemoveCipheredLSPlugin.js
+++ www/RemoveCipheredLSPlugin.js
@@ -2,11 +2,11 @@
 var originalOpenDatabase = window.sqlitePlugin.openDatabase;
 window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
 
-            return originalOpenDatabase.call(window.sqlitePlugin, options, successCallback, function() {
-		    sqlitePlugin.deleteDatabase(options, function() {
-			    window.sqlitePlugin.openDatabase(options, successCallback, errorCallback);
-		    }, function() {
-			    errorCallback();
-		    });
+    return originalOpenDatabase.call(window.sqlitePlugin, options, successCallback, function() {
+	    sqlitePlugin.deleteDatabase(options, function() {
+		    window.sqlitePlugin.openDatabase(options, successCallback, errorCallback);
+	    }, function() {
+		    errorCallback();
 	    });
+    });
 };
